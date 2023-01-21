b1facee5514a9e494512b78e8c6f2977fffc62d2
Agromos sp <101366474+Agromos0@users.noreply.github.com>
2023-01-21 12:30:51 -0600

    Update main-dylux.js

--- plugins/main-dylux.js
+++ plugins/main-dylux.js
@@ -1,19 +1,23 @@
-
 let handler = async (m, { conn }) => {
 
 m.reply(`
-≡  *TOXIC VIP ┃ SUPPORT*
-
+≡  *TOXIC ┃ SUPPORT*
 ◈ ━━━━━━━━━━━━━━━━━━━━ ◈
-▢ Grupo1
-https://chat.whatsapp.com/GNgvKstDSQg3xeEGdXTQMu
-
-▢ Grupo2
-https://chat.whatsapp.com/ISUYtyZAzFLEvuNMtFiN7C
-
-
+▢ Grupo *1*
+https://chat.whatsapp.com/IO9jmpI72ejHiN4unRZleU
+▢ Grupo *2*
+https://chat.whatsapp.com/CDUqNRu5Kh5KY5uqQI0BKE
+▢ Grupo *3* 
+https://chat.whatsapp.com/F0JTTyZ3hsoL7OlU8TEpuH
 ◈ ━━━━━━━━━━━━━━━━━━━━ ◈
-Numero de soporte= was.me/523931678470
+▢ soporte
+• +523931678470
+▢ *Telegram*
+• @@ANGELYT2
+ ▢ *PayPal*
+• hola2344lwy@gmail.com 
+▢ *ig*
+• @angelsdyt2422`)
 
 }
 handler.help = ['support']
