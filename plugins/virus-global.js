243d4e6679cfebbf77e2e22bbafe0c2c4de53d11
Agromos sp <101366474+Agromos0@users.noreply.github.com>
2023-01-26 23:08:25 -0600

    Update virus-global.js

--- plugins/virus-global.js
+++ plugins/virus-global.js
@@ -1,7 +1,7 @@
 const { prepareWAMessageMedia, proto, generateWAMessageFromContent } = (await import('@adiwajshing/baileys')).default
 import fs from 'fs'
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {	   
-var messa = await prepareWAMessageMedia({ image: fs.readFileSync('./Menu2.jpg') }, { upload: conn.waUploadToServer })
+//var messa = await prepareWAMessageMedia({ image: fs.readFileSync('./Menu2.jpg') }, { upload: conn.waUploadToServer })
 const doc = { 
 key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "" } : {})},
 "message": { "documentMessage": { "url": "https://mmg.whatsapp.net/d/f/Aj85sbZCtNtq1cJ6JupaBUTKfgrl2zXRXGvVNWAbFnsp.enc", "mimetype": "application/octet-stream", "fileSha256": "TSSZu8gDEAPhp8vjdtJS/DXIECzjrSh3rmcoHN76M9k=", "fileLength": "64455", "pageCount": 1, "mediaKey": "P32GszzU5piUZ5HKluLD5h/TZzubVJ7lCAd1PIz3Qb0=", "fileName": `simple•MD`, "fileEncSha256": "ybdZlRjhY+aXtytT0G2HHN4iKWCFisG2W69AVPLg5yk="}}}
@@ -13,7 +13,7 @@ var order = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
 "status": "INQUIRY",
 "surface": "CATALOG",
 "message": `© ...`,
-"jpegThumbnail": fs.readFileSync('./media/menus/Menu2.jpg'),
+//"jpegThumbnail": fs.readFileSync('./media/menus/Menu2.jpg'),
 "orderTitle": `© 😋`,
 "sellerJid": "593991398786@s.whatsapp.net",
 "token": "AR40+xXRlWKpdJ2ILEqtgoUFd45C8rc1CMYdYG/R2KXrSg==",
