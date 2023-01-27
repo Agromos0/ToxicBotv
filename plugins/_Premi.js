7b72f4b8478f556857393662bfb0f8cca8255fbe
Agromos sp <101366474+Agromos0@users.noreply.github.com>
2023-01-27 00:11:15 -0600

    Update _Premi.js

--- plugins/_Premi.js
+++ plugins/_Premi.js
@@ -5,8 +5,9 @@ let handler = async function (m, { conn, __dirname }) {
 let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
   
 m.reply(`
-*≡ QUIERES SER PREMIUN*
-▢ PRECIO 2 USD
+*≡ QUIERES SER PREMIUN TENDRAS MEJOR POSION QUE OTROS COMO METER EL BOT A TODOS TUS GRUPOS*
+* AL COMPRAR EL PREMIUN SE TE DAN 1000 DIAMANTES QUE TE SIRVEN PARA METER AQUILAR EL BOT ATUS GRUPOS POR SIERTO TIEMPO*
+_*PRECIO 2 USD*_
 •TE CONVERTIRAS EN PREMIUM  CUANDO TU PAGO SE RECIBA
 •AYUDARIAS MUCHO EN LA FUNCIONES  DEL BOT
 • TODO EL DINERO QUE DONES O DE COMPRAS PREMIUN  SE INVIERTE EN ERRAMIENTAS PARA  EL BOT
@@ -23,6 +24,6 @@ PIDELE A wa.me/523931678470 el metodo por el que vas a pagar el premium
 
 handler.help = ['script']
 handler.tags = ['main']
-handler.command = ['serpremiun', 'pm', 'comprarpremiun'] 
+handler.command = ['serpremiun', 'buyprem', 'comprarpremiun'] 
 
 export default handler
